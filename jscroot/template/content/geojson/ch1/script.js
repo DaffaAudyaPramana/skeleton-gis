b5c4c20b9f044ef5592d4b9f42bd0451971ff08a
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-25 21:14:49 +0700

    update ch 1 js

--- jscroot/template/content/geojson/ch1/script.js
+++ jscroot/template/content/geojson/ch1/script.js
@@ -5,17 +5,19 @@ export function main() {
         const coordinatesCell = row.insertCell(1);
         const typeCell = row.insertCell(2);
 
+        // Set nameCell based on properties
+        if (feature.properties.Alamat && feature.properties.Jalan) {
+            nameCell.innerText = `${feature.properties.Point}, ${feature.properties.Alamat}, ${feature.properties.Jalan}`;
+        } else if (feature.properties.Alamat) {
+            nameCell.innerText = `${feature.properties.Point}, ${feature.properties.Alamat}`;
+        } else if (feature.properties.Jalan) {
+            nameCell.innerText = `${feature.properties.Point}, ${feature.properties.Jalan}`;
+        } else {
+            nameCell.innerText = feature.properties.Point;
+        }
+
         coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
         typeCell.innerText = feature.geometry.type;
-
-        // Check for properties and set nameCell accordingly
-        if (feature.properties.Alamat && feature.properties.Jalan) {
-            nameCell.innerText = `${feature.properties.Alamat}, ${feature.properties.Jalan}`;
-        } else if (feature.properties.Alamat) {
-            nameCell.innerText = feature.properties.Alamat;
-        } else if (feature.properties.Jalan) {
-            nameCell.innerText = feature.properties.Jalan;
-        }
     }
 
     const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
