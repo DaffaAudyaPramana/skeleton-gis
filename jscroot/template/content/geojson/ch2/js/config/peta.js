a82fdd32c6305b332df47610cb5d84d7ba84ff3a
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-01-25 21:28:31 +0700

    update auto kordinat peta

--- jscroot/template/content/geojson/ch2/js/config/peta.js
+++ jscroot/template/content/geojson/ch2/js/config/peta.js
@@ -9,7 +9,7 @@ import {container} from 'https://jscroot.github.io/element/croot.js';
 
 const attributions = '';
 
-const place = [103.81128613262871,1.3247366000091603];
+const place = [107.54180787870939, -6.873533406322835];
 
 export let idmarker = {id:1};
 
